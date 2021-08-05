b92833105ea5edd502376b15e452d3f8c9a6ec0f
Kare Nuorteva <kare.nuorteva@me.com>
2021-08-05 22:02:08 +0000

    feat: Add hello9 program that prints Hello, World!

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <u.h>
+#include <libc.h>
+
+void
+main(void)
+{
+    print("Hello, World!\n");
+    exits(0);
+}
---
+++ mkfile
@@ -0,0 +1,8 @@
+</$objtype/mkfile
+
+TARG=hello9
+OFILES=\
+    main.$O
+BIN=/$objtype/bin
+
+</sys/src/cmd/mkone
